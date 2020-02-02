d98e0b216162ce14ac9180d2786baf1e8d1cabeb
wisestMe <wyhzest@gmail.com>
2020-02-02 20:29:34 +0100

    set pattern for confirm password

--- Assignment/form.js
+++ Assignment/form.js
@@ -1,13 +1,5 @@
 let inputs = document.getElementsByTagName('input');
 
-//let getValue = document.querySelector('get-value');
-
-// getValue.addEventListener('click', function(){
-// 	for (let item of inputs) {
-// 	console.log(item.value);
-// }
-// })
-
 let button = document.querySelector('button');
 
 button.addEventListener("click", function(event) {
@@ -15,4 +7,20 @@ button.addEventListener("click", function(event) {
 	for (let item of inputs) {
  	console.log(item.value);
 }
+});
+
+// VALIDATE CONFIRM PASSWORD FIELD
+let password = document.getElementById("password"),
+	confirm = document.getElementById("confirm");
+
+confirm.addEventListener('input', function() {
+	confirm.setAttribute('pattern', password.value);
+});
+
+confirm.addEventListener('change', function() {
+	confirm.setAttribute('pattern', password.value);
+});
+
+password.addEventListener('change', function() {
+	confirm.setAttribute('pattern', password.value);
 });
